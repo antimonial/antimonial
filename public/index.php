23d2aeb6f2bb547c3c76e59581dd9134e2ba67ab
Laureano Lamonega <laureanolamonega@gmail.com>
2026-07-14 19:02:57 -0300

    feat: public front controller and Apache .htaccess

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+define('ROOT_PATH', __DIR__ . '/..');
+require ROOT_PATH . '/vendor/autoload.php';
+
+Antimonial\Core\Config::load('app');
+Antimonial\Core\Config::load('database');
+Antimonial\Core\ErrorHandler::enableDebug((bool) env('APP_DEBUG', false));
+
+$app = require ROOT_PATH . '/bootstrap/app.php';
+$app->run();
