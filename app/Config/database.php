8c1869514d0752137624c65b4971c22d92783d62
Laureano Lamonega <laureanolamonega@gmail.com>
2026-07-14 19:02:57 -0300

    feat: application config (app + database)

---
+++ app/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+        'mysql' => [
+            'host'     => env('DB_HOST', '127.0.0.1'),
+            'port'     => env('DB_PORT', 3306),
+            'database' => env('DB_NAME', 'myapp'),
+            'username' => env('DB_USER', 'root'),
+            'password' => env('DB_PASS', ''),
+        ],
+    ],
+];
