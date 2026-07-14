6d990ed9b5559a6d48961e6dc691937dc1f8249f
Laureano Lamonega <laureanolamonega@gmail.com>
2026-07-14 19:11:32 -0300

    feat: load .env via Antimonial DotEnv in front controller

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 define('ROOT_PATH', __DIR__ . '/..');
 require ROOT_PATH . '/vendor/autoload.php';
 
+Antimonial\Core\DotEnv::load(ROOT_PATH . '/.env');
 Antimonial\Core\Config::load('app');
 Antimonial\Core\Config::load('database');
 Antimonial\Core\ErrorHandler::enableDebug((bool) env('APP_DEBUG', false));
