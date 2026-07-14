6a1582eb8d30b47fee32cd1ce0d747c3eb0947dd
Laureano Lamonega <laureanolamonega@gmail.com>
2026-07-14 19:02:57 -0300

    feat: example User model

---
+++ app/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Antimonial\Model\Model;
+
+class User extends Model
+{
+    // The table name defaults to "users" (guessed from the class name).
+    // Set $table explicitly for irregular plurals, e.g.:
+    // protected static string $table = 'users';
+}
