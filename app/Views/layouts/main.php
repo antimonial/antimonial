bbda10aa0a940943a110e34a6222afa01dcb5443
laureano <laureanolamonega@gmail.com>
2026-07-18 20:12:02 +0000

    feat: add static assets and shared layout with error/flash block

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -4,8 +4,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Antimonial')</title>
+    <link rel="stylesheet" href="/assets/css/app.css">
 </head>
 <body>
+    @if(!empty(errors()))
+        <div class="flash" id="flash">
+            <ul>
+            @foreach(errors() as $fieldErrors)
+                @foreach($fieldErrors as $message)
+                    <li>{{ e($message) }}</li>
+                @endforeach
+            @endforeach
+            </ul>
+        </div>
+    @endif
+
     {{{ $content }}}
 </body>
 </html>
